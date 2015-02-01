0c80eb3ef8595b70e607423593c8f1f3752f5e45
Ross McIlwaine <rdm0423@gmail.com>
2015-01-31 18:06:21 -0700

    Completed FizzBuzz

--- FizzBuzz-Objc/FizzBuzz-Objc/AppDelegate.m
+++ FizzBuzz-Objc/FizzBuzz-Objc/AppDelegate.m
@@ -11,11 +11,80 @@
 @implementation AppDelegate
 
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
     
     // Insert code here
     
+    for (int i = 1; i <= 100; i++)
+    {
+        BOOL buzz = [self isBuzzed:i];
+        BOOL fizz = [self isFizzed:i];
+        
+        if (buzz && !fizz)
+        {
+            NSLog(@"\nBuzz");
+        }
+        else if (fizz && !buzz)
+        {
+            NSLog(@"\nFizz");
+        }
+        else if (fizz && buzz)
+        {
+            NSLog(@"\nFizzBuzz");
+        }
+        else
+        {
+            NSLog(@"\n%d", i);
+        }
+    }
+    
+    
     return YES;
+}
+
+// Method Statements
+
+
+-(BOOL) isBuzzed:(NSInteger)number
+{
+   
+    BOOL isBuzzed;
+    if (number % 3 == 0)
+    {
+        isBuzzed = YES;
+    }
+    else if ([[NSString stringWithFormat:@"%ld", number] containsString:@"3"])
+    {
+        isBuzzed = YES;
+    }
+
+    else
+    {
+        isBuzzed = NO;
+    }
+    
+    return isBuzzed;
+}
+
+
+-(BOOL) isFizzed:(NSInteger)number
+{
+    BOOL isFizzed;
+    if (number % 5 == 0)
+    {
+        isFizzed = YES;
+    }
+    else if ([[NSString stringWithFormat:@"%ld", number] containsString:@"5"])
+    {
+        isFizzed = NO;
+    }
+    else
+    {
+        isFizzed = NO;
+    }
+    
+    return isFizzed;
 }
 
 @end
